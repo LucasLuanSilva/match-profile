fe27fb872c4399410eb608193b2ee0c3c8a364c6
Lucas Luan Silva <lucasluansilva00@hotmail.com>
2021-08-24 22:54:55 -0300

    first commit

---
+++ backend/src/database/migrations/1629854406031-CreateUsuariosEmpresariais.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsuariosEmpresariais1629854406031 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
