599ef2e79ebc8bb2bb8e2f52fa0266e28dd702d5
Lucas Luan Silva <lucasluansilva00@hotmail.com>
2021-10-30 10:28:50 -0300

    Criado migration e entidade respostas

---
+++ backend/src/database/migrations/1635599661026-CreateRespostas.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRespostas1635599661026 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'respostas',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'questoes_id',
+            type: 'uuid',
+            isNullable: false
+          },
+          {
+            name: 'resposta',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'correta',
+            type: 'int',
+            isNullable: false,
+            default: 0,
+            comment: '0 - Não / 1 - Sim'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FK_respostas_questoes_id',
+            referencedTableName: 'questoes',
+            referencedColumnNames: ['id'],
+            columnNames: ['questoes_id']
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('respostas');
+  }
+
+}
---
+++ backend/src/entities/Resposta.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+import Questao from "./Questao";
+
+@Entity('respostas')
+class Resposta {
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  readonly questoes_id: string;
+  @JoinColumn({ name: 'questoes_id' })
+  @ManyToOne(() => Questao)
+  questao: Questao
+
+  @Column()
+  resposta: string;
+
+  @Column()
+  correta: number;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default Resposta;
