de0b8a3ed01f957531ac588e2db22177c2d1e01b
Lucas Luan Silva <lucasluansilva00@hotmail.com>
2021-10-30 12:48:03 -0300

    Criado entidade e migration respostas_preenchidas

---
+++ backend/src/database/migrations/1635608222562-CreateRespostasPreenchidas.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRespostasPreenchidas1635608222562 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'respostas_preenchidas',
+        columns: [
+          {
+            name: 'questoes_id',
+            type: 'uuid',
+            isNullable: false
+          },
+          {
+            name: 'respostas_id',
+            type: 'uuid',
+            isNullable: false
+          },
+          {
+            name: 'testes_atribuidos_id',
+            type: 'uuid',
+            isNullable: false
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FK_respostas_preenchidas_questoes_id',
+            referencedTableName: 'questoes',
+            referencedColumnNames: ['id'],
+            columnNames: ['questoes_id']
+          },
+          {
+            name: 'FK_respostas_preenchidas_respostas_id',
+            referencedTableName: 'respostas',
+            referencedColumnNames: ['id'],
+            columnNames: ['respostas_id']
+          },
+          {
+            name: 'FK_respostas_preenchidas_testes_atribuidos_id',
+            referencedTableName: 'testes_atribuidos',
+            referencedColumnNames: ['id'],
+            columnNames: ['testes_atribuidos_id']
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('respostas_preenchidas');
+  }
+
+}
---
+++ backend/src/entities/RespostaPreenchida.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import Questao from "./Questao";
+import Resposta from "./Resposta";
+import TesteAtribuido from "./TesteAtribuido";
+
+@Entity('respostas_preenchidas')
+class RespostaPreenchida {
+
+  @Column()
+  readonly questoes_id: string;
+  @JoinColumn({ name: 'questoes_id' })
+  @ManyToOne(() => Questao)
+  questao: Questao
+
+  @Column()
+  readonly respostas_id: string;
+  @JoinColumn({ name: 'respostas_id' })
+  @ManyToOne(() => Resposta)
+  resposta: Resposta
+
+  @Column()
+  readonly testes_atribuidos_id: string;
+  @JoinColumn({ name: 'testes_atribuidos_id' })
+  @ManyToOne(() => TesteAtribuido)
+  testeAtribuido: TesteAtribuido
+
+}
+
+export default RespostaPreenchida;
